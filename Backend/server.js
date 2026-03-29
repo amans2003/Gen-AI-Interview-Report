9fade70d3dd4af0684cfcc3f6240c8183ea9c1c5
Aman <amaninternsingh2003@gmail.com>
2026-03-29 21:01:03 +0530

    Fix: Export app for Vercel backend

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,10 @@ const connectToDB = require("./src/config/database")
 connectToDB()
 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+if (process.env.NODE_ENV !== "production") {
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000")
+    })
+}
+
+module.exports = app
